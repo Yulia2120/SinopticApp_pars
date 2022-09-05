b0334b6180bf6a6e1c11e571a74dae43ecc2c9db
Yulia2120 <ket-dz@rambler.ru>
2022-09-05 14:30:59 +0300

    Добавьте файлы проекта.

---
+++ SinopticApp_pars/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var result = Parsing(url: "https://sinoptik.ua/");
+
+object Parsing(string url)
+{
+    try
+    {
+        using(HttpClientHandler hdl = new HttpClientHandler()) // Обработчик веб- запросов
+        {
+            using(var client = new HttpClient(hdl))
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    if(response.IsSuccessStatusCode)  //Возвращает значение, указывающее, завершился ли успешно HTTP-ответ.
+                    {
+                        var html = response.Content.ReadAsStringAsync().Result;  //Сериализация содержимого HTTP в строку в качестве асинхронной операции.
+                        if (!string.IsNullOrEmpty(html))
+                        {
+                            {
+                                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                                doc.LoadHtml(html);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    catch (Exception ex)
+    { Console.WriteLine(ex.Message); }
+}
